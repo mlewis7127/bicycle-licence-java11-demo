5c37d3d125e9c2c6057c7a3b40c4673ff1f090eb
Matt Lewis <matt.lewis@ibm.com>
2025-11-06 12:01:48 +0000

    remove unused dependencies and disable JPA auto configuration

--- src/main/java/uk/gov/dvla/poc/BicycleLicenceApplication.java
+++ src/main/java/uk/gov/dvla/poc/BicycleLicenceApplication.java
@@ -3,7 +3,10 @@ package uk.gov.dvla.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 //@EnableOAuth2Sso
 public class BicycleLicenceApplication {
     public static void main(String[] args) {
